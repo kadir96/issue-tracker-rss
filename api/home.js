a882ce0ff0ec28ec2d6d1709e0b9721d441f8242
Abdulkadir Poyraz <kadirpoyraz96@gmail.com>
2020-05-14 21:28:15 +0300

    add example url to home

--- api/home.js
+++ api/home.js
@@ -1,3 +1,6 @@
 module.exports = async (req, res) => {
-    res.send("You need to provide repo name with request params in url :)")
+    res.send(`
+    You need to provide repo name with request params in url :)<br>
+    Example: <a href="https://issue-tracker-rss.now.sh/mattermost/mattermost-server?labels=Help%20Wanted,Tech%2FGo">https://issue-tracker-rss.now.sh/mattermost/mattermost-server?labels=Help%20Wanted,Tech%2FGo</a>
+    `)
 }
