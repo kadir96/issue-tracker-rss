fc8e37d54cc90601db9c0cd54b4882e8368e104a
Abdulkadir Poyraz <kadirpoyraz96@gmail.com>
2020-05-14 22:41:42 +0300

    request github api with token not to face rate limit add issue content to output

--- api/issues.js
+++ api/issues.js
@@ -4,9 +4,15 @@ const axios = require('axios').default;
 
 
 module.exports = async (req, res) => {
-    let { repo, ...params } = req.query;
+    let {repo, ...params} = req.query;
 
-    let response = await axios.get(`https://api.github.com/repos/${repo}/issues?${querystring.encode(params)}`);
+    let response = await axios.get(
+        `https://api.github.com/repos/${repo}/issues?${querystring.encode(params)}`,
+        {
+            auth: {
+                username: process.env.GITHUB_TOKEN
+            }
+        });
 
     const feed = new Feed({
         title: `${repo} Issues`,
@@ -15,6 +21,7 @@ module.exports = async (req, res) => {
     response.data.forEach((data) => {
         feed.addItem({
             title: data.title,
+            description: data.body,
             id: data.url,
             url: data.url,
             date: new Date(Date.parse(data.created_at)),
